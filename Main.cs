4df20176c1f6d455089183524daae2c1730bfb6e
AntoninaVish <vishivtonya@gmail.com>
2023-03-04 22:00:38 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPICreatingWPFProject
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            
+            var window = new MainView(commandData); //вызываем окно
+            window.ShowDialog(); //вызываем окно для того чтобы оно появилось
+           
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ MainViewViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPICreatingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPICreatingWPFProject
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SelectCommand { get; }
+
+        //создаем конструктор с помощью снипита ctor чтобы вызвать конструктор нажимаем tab два раза
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            //сохраняем в отдельное поле, для того чтобы потом вызвать Document, UIDocument; преобразовывать реферес в элементы и т.д.
+            _commandData = commandData;
+
+            SelectCommand = new DelegateCommand(OnSelectCommand);
+        }
+
+        public event EventHandler HideRequest;
+
+        private void RaiseHideRequest()
+        {
+            HideRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+       
+        public event EventHandler ShowRequest;
+
+        private void RaiseShowRequest() // при вызове RaiseShowRequest будет вызываться данный event
+        {
+           ShowRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnSelectCommand()
+        {
+            RaiseHideRequest();  //будем временно скрывать окно, когда запускается команда выбора элемента
+
+            Element oElement = SelectionUtils.PickObject(_commandData);
+
+            TaskDialog.Show("Сообщение", $"ID: {oElement.Id}"); //выводим идентификатор
+
+            RaiseShowRequest();
+        }
+
+       
+    }
+}
